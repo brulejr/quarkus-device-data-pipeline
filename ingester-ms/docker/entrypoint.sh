e16622d4bf5b424191d77fded39365f18a71d006
Jon Brule <brulejr@tgmail.com>
2025-07-27 15:15:36 -0400

    Fixes secrets in docker-compose.yml

--- ingester-ms/docker/entrypoint.sh
+++ ingester-ms/docker/entrypoint.sh
@@ -2,9 +2,7 @@
 
 export INCOMING_MQTT_HOST=$(cat /run/secrets/incoming_mqtt_host)
 export INCOMING_MQTT_PORT=$(cat /run/secrets/incoming_mqtt_port)
-export OUTGOING_RABBITMQ_HOST=$(cat /run/secrets/outgoing_rabbitmq_host)
-export OUTGOING_RABBITMQ_PORT=$(cat /run/secrets/outgoing_rabbitmq_port)
-export OUTGOING_RABBITMQ_USERNAME=$(cat /run/secrets/outgoing_rabbitmq_username)
-export OUTGOING_RABBITMQ_PASSWORD=$(cat /run/secrets/outgoing_rabbitmq_password)
+export RABBITMQ_CLIENT_USERNAME=$(cat /run/secrets/rabbitmq_client_username)
+export RABBITMQ_CLIENT_PASSWORD=$(cat /run/secrets/rabbitmq_client_password)
 
 java -jar /app/ingester-ms.jar --spring.config.additional-location=file:/config/app
