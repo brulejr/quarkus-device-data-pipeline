a2a66570bf15e891d9c733df08885bafd7abf47b
Jon Brule <brulejr@tgmail.com>
2025-07-30 22:36:04 -0400

    Adds Rtl433Ingester to ingester-ms

---
+++ ingester-ms/src/main/kotlin/io/jrb/labs/device/messaging/Rtl433Ingester.kt
@@ -0,0 +1,48 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2025 Jon Brule <brulejr@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.jrb.labs.device.messaging
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.jrb.labs.common.logging.LoggerDelegate
+import io.jrb.labs.messages.RawMessageSource
+import io.jrb.labs.messages.Rtl433Message
+import io.jrb.labs.messages.datatypes.Rtl433Data
+import jakarta.enterprise.context.ApplicationScoped
+import org.eclipse.microprofile.reactive.messaging.Incoming
+import org.eclipse.microprofile.reactive.messaging.Outgoing
+
+@ApplicationScoped
+class Rtl433Ingester(private val objectMapper: ObjectMapper) {
+
+    private val log by LoggerDelegate()
+
+    @Incoming("rtl433-in")
+    @Outgoing("rtl433-message")
+    fun process(payload: String): Rtl433Message {
+        val rtl433Data = objectMapper.convertValue(payload, Rtl433Data::class.java)
+        log.info("RTL433 payload: $rtl433Data")
+        return Rtl433Message(source = RawMessageSource.RTL433, payload = rtl433Data)
+    }
+
+}
