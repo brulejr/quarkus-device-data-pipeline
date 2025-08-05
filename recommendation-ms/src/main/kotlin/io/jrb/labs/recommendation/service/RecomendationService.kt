cd99ecccc3e8ed2dba7a690ad23b6336af9c64f8
Jon Brule <brulejr@tgmail.com>
2025-08-05 14:16:08 -0400

    Adds RecommendationService

--- recommendation-ms/src/main/kotlin/io/jrb/labs/recommendation/service/RecomendationService.kt
+++ recommendation-ms/src/main/kotlin/io/jrb/labs/recommendation/service/RecomendationService.kt
@@ -21,21 +21,39 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package io.jrb.labs.recommendation.messaging
+package io.jrb.labs.recommendation.service
 
+import io.jrb.labs.common.eventbus.SystemEventBus
+import io.jrb.labs.common.logging.LoggerDelegate
+import io.jrb.labs.common.service.ControllableService
 import io.jrb.labs.messages.Rtl433Message
-import io.jrb.labs.recommendation.service.RecomendationService
-import io.vertx.core.json.JsonObject
+import io.quarkus.runtime.Startup
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
 import jakarta.enterprise.context.ApplicationScoped
-import org.eclipse.microprofile.reactive.messaging.Incoming
 
+@Startup
 @ApplicationScoped
-class Rtl433MessageSubscriber(private val recomendationService: RecomendationService) {
+class RecomendationService(
+    override var systemEventBus: SystemEventBus
+) : ControllableService() {
 
-    @Incoming("raw-message")
-    fun process(message: JsonObject) {
-        val rtl433Message = message.mapTo(Rtl433Message::class.java)
-        recomendationService.processRtl433Message(rtl433Message)
+    override val serviceName = "RecommendationService"
+
+    private val log by LoggerDelegate()
+
+    fun processRtl433Message(rtl433Message: Rtl433Message) {
+        log.info("rtl433Message: {}", rtl433Message)
+    }
+
+    @PostConstruct
+    override fun startup() {
+        super.startup()
+    }
+
+    @PreDestroy
+    override fun shutdown() {
+        super.startup()
     }
 
 }
